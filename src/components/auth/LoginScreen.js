b1e39e01e7a82f53d8eb6ec556fcfa06d1a94213
Leonardo Omaña <leotangram@gmail.com>
2020-09-02 16:10:04 -0500

    Primer dispatch de una acción al store

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,24 +1,45 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useDispatch } from 'react-redux'
 import { Link } from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
+import { login } from '../../actions/auth'
 
 const LoginScreen = props => {
+  const dispatch = useDispatch()
+
+  const [formValues, handleInputChange] = useForm({
+    email: 'leo@gmail.com',
+    password: '123456'
+  })
+
+  const { email, password } = formValues
+
+  const handleLogin = e => {
+    e.preventDefault()
+    dispatch(login(12345, 'Leonardo'))
+  }
+
   return (
     <>
       <h3 className="auth__title">Login</h3>
-      <form>
+      <form onSubmit={handleLogin}>
         <input
           type="text"
           placeholder="Email"
           name="email"
           className="auth__input"
           autoComplete="off"
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           type="password"
           placeholder="Password"
           name="password"
           className="auth__input"
+          value={password}
+          onChange={handleInputChange}
         />
         <button type="submit" className="btn btn-primary btn-block">
           Login
