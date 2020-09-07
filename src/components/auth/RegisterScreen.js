e60f606f14fb09b757b672e73e05922301df9140
Leonardo Omaña <leotangram@gmail.com>
2020-09-07 10:44:07 -0500

    Manejo de errores del formulario

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
+import validator from 'validator'
 import { useForm } from '../../hooks/useForm'
 
 const RegisterScreen = props => {
@@ -15,13 +16,32 @@ const RegisterScreen = props => {
 
   const handleRegister = e => {
     e.preventDefault()
-    console.log(name, email, password, confirmPassword)
+    if (isFormValid()) {
+      console.log('Formulario correcto')
+    }
+  }
+
+  const isFormValid = () => {
+    if (name.trim().length === 0) {
+      console.log('Name is required')
+      return false
+    } else if (!validator.isEmail(email)) {
+      console.log('Email is not valid')
+      return false
+    } else if (password !== confirmPassword || password.length < 5) {
+      console.log(
+        'Password shoul be at least 6 characters ant match each other'
+      )
+      return false
+    }
+    return true
   }
 
   return (
     <>
       <h3 className="auth__title">Register</h3>
       <form onSubmit={handleRegister}>
+        <div className="auth__alert-error">Hola mundo</div>
         <input
           type="text"
           placeholder="Name"
