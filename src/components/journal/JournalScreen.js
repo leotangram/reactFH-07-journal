22ac10bab0b40b513b9ed58dfec9c1f1ecb3d719
Leonardo Omaña <leotangram@gmail.com>
2020-09-09 10:40:39 -0500

    Notes Reducer

--- src/components/journal/JournalScreen.js
+++ src/components/journal/JournalScreen.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useSelector } from 'react-redux'
 import Sidebar from './Sidebar'
 import NothingSelected from './NothingSelected'
 import NoteScreen from '../notes/NoteScreen'
 
 const JournalScreen = props => {
+  const { active } = useSelector(state => state.notes)
+
   return (
     <div className="journal__main-content">
       <Sidebar />
-      <main>
-        {/* <NothingSelected /> */}
-        <NoteScreen />
-      </main>
+      <main>{active ? <NoteScreen /> : <NothingSelected />}</main>
     </div>
   )
 }
--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import { authReducer } from '../reducers/authReducer'
 import { uiReducer } from '../reducers/uiReducer'
+import { notesReducer } from '../reducers/notesReducer'
 
 const composeEnhancers =
   (typeof window !== 'undefined' &&
@@ -10,7 +11,8 @@ const composeEnhancers =
 
 const reducers = combineReducers({
   auth: authReducer,
-  ui: uiReducer
+  ui: uiReducer,
+  notes: notesReducer
 })
 
 export const store = createStore(
