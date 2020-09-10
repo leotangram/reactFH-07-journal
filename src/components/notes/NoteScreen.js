2c980af96cf6b6230b4ac9a54c5b71e54010ce3b
Leonardo Omaña <leotangram@gmail.com>
2020-09-10 16:56:07 -0500

    Cambiar la nota activa

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import PropTypes from 'prop-types'
 import { useSelector } from 'react-redux'
 import { useForm } from '../../hooks/useForm'
@@ -6,9 +6,18 @@ import NotesAppBar from './NotesAppBar'
 
 const NoteScreen = props => {
   const { active: note } = useSelector(state => state.notes)
-  const [formValues, handleInputChange] = useForm(note)
+  const [formValues, handleInputChange, reset] = useForm(note)
 
   const { body, title } = formValues
+
+  const activeId = useRef(note.id)
+
+  useEffect(() => {
+    if (note.id !== activeId.current) {
+      reset(note)
+      activeId.current = note.id
+    }
+  }, [note, reset])
 
   return (
     <div className="notes__main-content">
