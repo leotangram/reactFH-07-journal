fb3930fd2b6112b68d571ed03aed310d09f03f02
Leonardo Omaña <leotangram@gmail.com>
2020-09-11 15:11:30 -0500

    Cloudinary SDK - delete image

--- src/tests/herlpers/fileUpload.test.js
+++ src/tests/herlpers/fileUpload.test.js
@@ -1,7 +1,14 @@
+import cloudinary from 'cloudinary'
 import { fileUpload } from '../../helpers/fileUpload'
 
+cloudinary.config({
+  cloud_name: 'dfilni70m',
+  api_key: '916266836923311',
+  api_secret: '1d9ssFPvwFGVOigGwKk6jHPgNc0'
+})
+
 describe('fileUpload file', () => {
-  test('should load a file & return url', async () => {
+  test('should load a file & return url', async done => {
     const response = await fetch(
       'https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png'
     )
@@ -10,6 +17,13 @@ describe('fileUpload file', () => {
     const url = await fileUpload(file)
 
     expect(typeof url).toBe('string')
+
+    const segments = url.split('/')
+    const imageId = segments[segments.length - 1].replace('.png', '')
+
+    cloudinary.v2.api.delete_resources(imageId, {}, () => {
+      done()
+    })
   })
 
   test('should return an error', async () => {
