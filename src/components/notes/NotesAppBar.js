a97c6d72ed388063a5e48726ed67b564da7f5e7f
Leonardo Omaña <leotangram@gmail.com>
2020-09-11 11:16:54 -0500

    Subir imágen a cloudinary

--- src/components/notes/NotesAppBar.js
+++ src/components/notes/NotesAppBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { useDispatch, useSelector } from 'react-redux'
-import { startSaveNotes } from '../../actions/notes'
+import { startSaveNotes, startUploading } from '../../actions/notes'
 
 const NotesAppBar = props => {
   const dispatch = useDispatch()
@@ -11,11 +11,31 @@ const NotesAppBar = props => {
     dispatch(startSaveNotes(note))
   }
 
+  const handlePictureClick = () => {
+    document.querySelector('#fileSelector').click()
+  }
+
+  const handleFileChange = e => {
+    const file = e.target.files[0]
+    if (file) {
+      dispatch(startUploading(file))
+    }
+  }
+
   return (
     <div className="notes__appbar">
       <span>28 de Agosto 2020</span>
+      <input
+        id="fileSelector"
+        type="file"
+        name="file"
+        style={{ display: 'none' }}
+        onChange={handleFileChange}
+      />
       <div>
-        <button className="btn">Picture</button>
+        <button className="btn" onClick={handlePictureClick}>
+          Picture
+        </button>
         <button className="btn" onClick={handleSave}>
           Save
         </button>
---
+++ src/helpers/fileUpload.js
@@ -0,0 +1,17 @@
+export const fileUpload = async file => {
+  const cloudUrl = 'https://api.cloudinary.com/v1_1/dfilni70m/upload'
+  const formData = new FormData()
+  formData.append('upload_preset', 'react-journal')
+  formData.append('file', file)
+  try {
+    const response = await fetch(cloudUrl, { method: 'POST', body: formData })
+    if (response.ok) {
+      const cloudResponse = await response.json()
+      return cloudResponse.secure_url
+    } else {
+      throw await response.json()
+    }
+  } catch (error) {
+    throw error
+  }
+}
