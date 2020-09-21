ffb5cd1f87a3dbb568ec490b3c7abd65866800b6
Leonardo Omaña <leotangram@gmail.com>
2020-09-21 14:37:24 -0500

    Pruebas en startLoadingNotes y startSaveNote

--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -1,19 +1,29 @@
 import configureStore from 'redux-mock-store'
 import thunk from 'redux-thunk'
-import { startNewNote } from '../../actions/notes'
+import {
+  startLoadingNotes,
+  startNewNote,
+  startSaveNotes
+} from '../../actions/notes'
 import { db } from '../../firebase/firebaseConfig'
 import { types } from '../../types/types'
 
 const middlewares = [thunk]
 const mockStore = configureStore(middlewares)
 
-const store = mockStore({
+const initState = {
   auth: {
     uid: 'TESTING'
   }
-})
+}
+
+let store = mockStore(initState)
 
 describe('notes actions test', () => {
+  beforeEach(() => {
+    store = mockStore(initState)
+  })
+
   test('should create a new note "startNewNote"', async () => {
     await store.dispatch(startNewNote())
     const actions = store.getActions()
@@ -39,5 +49,41 @@ describe('notes actions test', () => {
 
     const docId = actions[0].payload.id
     await db.doc(`/TESTING/journal/notes/${docId}`).delete()
+  })
+
+  test('should load notes', async () => {
+    await store.dispatch(startLoadingNotes('TESTING'))
+    const actions = store.getActions()
+    expect(actions[0]).toEqual({
+      type: types.notesLoad,
+      payload: expect.any(Array)
+    })
+
+    const expected = {
+      id: expect.any(String),
+      title: expect.any(String),
+      body: expect.any(String),
+      date: expect.any(Number)
+    }
+
+    expect(actions[0].payload[0]).toMatchObject(expected)
+  })
+
+  test('should startSaveNote update note', async () => {
+    const note = {
+      id: 'Xf6IRMdTScTyMDgcyHbO',
+      title: 'titulo',
+      body: 'body'
+    }
+
+    await store.dispatch(startSaveNotes(note))
+
+    const actions = store.getActions()
+
+    expect(actions[0].type).toBe(types.notesUpdated)
+
+    const docRef = await db.doc(`/TESTING/journal/notes/${note.id}`).get()
+
+    expect(docRef.data().title).toBe(note.title)
   })
 })
